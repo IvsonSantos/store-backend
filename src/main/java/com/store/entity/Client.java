4117715891faf46c59c18df4a3999314343ea83d
ivsonsantos <Belgique@2024>
2021-07-15 10:25:55 +0200

    Entities created

---
+++ src/main/java/com/store/entity/Client.java
@@ -0,0 +1,29 @@
+package com.store.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+}
